925b51716a54bea17b755eb3dd8ee233d81dcceb
moeezramay <moeezramay@192.168.0.140>
2023-12-18 18:43:18 +0500

    V34

--- src/lib/db2.js
+++ src/lib/db2.js
@@ -4,9 +4,6 @@ export default async function checkDatabaseConnection() {
   try {
     // Attempt to execute a simple query to check the connection
     await sql`SELECT 1`;
-
-    console.log("Database connected successfully!");
-    return "success";
   } catch (error) {
     console.error("Error connecting to the database:", error);
     return "faliure";
