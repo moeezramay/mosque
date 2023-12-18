296f6848104c24fe7adc6c4c39b51b25326d775f
moeezramay <moeezramay@Moeezs-MacBook-Pro.local>
2023-12-18 15:48:09 +0500

    V29

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -4,7 +4,7 @@ import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
