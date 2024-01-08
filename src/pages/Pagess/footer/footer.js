9e8cd6d4079b985c52c7467be4e0a13908674898
moeezramay <moeezramay@192.168.0.140>
2024-01-08 23:53:11 +0500

    footer removed

--- src/pages/Pagess/footer/footer.js
+++ src/pages/Pagess/footer/footer.js
@@ -79,133 +79,8 @@ export default function Footer() {
             </span>
           </div>
         </div>
-        <div className="others-parent-footer">
-          <div className="visit-other-container-footer">
-            <div className="visit-other-heading-footer">
-              {t("footer.visit")}
-            </div>
-            <div className="links-container-footer">
-              {/* Container 1 */}
-              <div className="container-1-footer">
-                <a
-                  href="https://www.afrointroductions.com"
-                  className="links-footer"
-                >
-                  AfroIntroductions.com
-                </a>
-                <a href="https://www.asiandating.com/" className="links-footer">
-                  AsianDating.com
-                </a>
-                <a href="https://www.bbwcupid.com/" className="links-footer">
-                  BBWCupid.com
-                </a>
-                <a href="https://www.blackcupid.com/" className="links-footer">
-                  BlackCupid.com
-                </a>
-                <a href="https://www.brazilcupid.com/" className="links-footer">
-                  BrazilCupid.com
-                </a>
-                <a
-                  href="https://www.cambodiancupid.com/"
-                  className="links-footer"
-                >
-                  CambodianCupid.com
-                </a>
-                <a
-                  href="https://www.caribbeancupid.com/"
-                  className="links-footer"
-                >
-                  CaribbeanCupid.com
-                </a>
-              </div>
-              {/* Container 2 */}
-              <div className="container-2-footer">
-                <a
-                  href="https://www.chinalovecupid.com/"
-                  className="links-footer"
-                >
-                  ChinaLoveCupid.com
-                </a>
-                <a
-                  href="https://www.colombiancupid.com/"
-                  className="links-footer"
-                >
-                  ColombianCupid.com
-                </a>
-                <a
-                  href="https://www.dominicancupid.com/"
-                  className="links-footer"
-                >
-                  DominicanCupid.com
-                </a>
-                <a href="https://www.elitecupid.com/" className="links-footer">
-                  EliteCupid.com
-                </a>
-                <a href="https://www.eurocupid.com/" className="links-footer">
-                  EuroCupid.com
-                </a>
-                <a
-                  href="https://www.filipinocupid.com/"
-                  className="links-footer"
-                >
-                  FilipinoCupid.com
-                </a>
-                <a
-                  href="https://www.hongkongcupid.com/"
-                  className="links-footer"
-                >
-                  HongKongCupid.com
-                </a>
-              </div>
-              {/* Container 3 */}
-              <div className="container-2-footer">
-                <a href="https://www.indiancupid.com/" className="links-footer">
-                  IndianCupid.com
-                </a>
-                <a
-                  href="https://www.indonesiancupid.com/"
-                  className="links-footer"
-                >
-                  IndonesianCupid.com
-                </a>
-                <a
-                  href="https://www.internationalcupid.com/"
-                  className="links-footer"
-                >
-                  InternationalCupid.com
-                </a>
-                <a
-                  href="https://www.interracialcupid.com/"
-                  className="links-footer"
-                >
-                  InterracialCupid.com
-                </a>
-                <a href="https://www.japancupid.com/" className="links-footer">
-                  JapanCupid.com
-                </a>
-                <a href="https://www.kenyancupid.com/" className="links-footer">
-                  KenyaCupid.com
-                </a>
-                <a href="https://www.koreancupid.com/" className="links-footer">
-                  KoreanCupid.com
-                </a>
-              </div>
-              {/* Container 4*/}
-              <div className="container-2-footer">
-                <a href="https://www.ukrainedate.com/" className="links-footer">
-                  UkranianDate.com
-                </a>
-                <a
-                  href="https://www.vietnamcupid.com/"
-                  className="links-footer"
-                >
-                  VietnamCupid.com
-                </a>
-              </div>
-            </div>
-          </div>
+        {/* <div className="others-parent-footer">
           <div className="legal-container-parent-footer">
-            {/* Company Legal Section */}
             <div className="legal-container-1-footer">
               <div className="legal-heading-footer">
                 {t("footer.legalHead1")}
@@ -265,7 +140,6 @@ export default function Footer() {
               </a>
             </div>
             <div className="legal-container-2-footer">
-              {/*  Legal Section */}
               <div className="legal-heading-footer">
                 {t("footer.legalHead3")}
               </div>
@@ -301,7 +175,6 @@ export default function Footer() {
               </a>
             </div>
             <div className="legal-container-2-footer">
-              {/*  Others Section */}
               <div className="legal-heading-2-footer">
                 {t("footer.legalHead4")}
               </div>
@@ -312,7 +185,7 @@ export default function Footer() {
               <div>{t("footer.rights")}</div>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
