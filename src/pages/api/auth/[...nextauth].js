665b3667f765a7cc7e84fc04199420e97c5ecb85
aleeizhere <alistpaulscr7@gmail.com>
2024-02-25 17:46:08 +0500

    updated redirect in next auth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
+
   providers: [
     GoogleProvider({
       clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
@@ -10,5 +11,10 @@ export default NextAuth({
     //   redirect: "https://www.mosquematch.com/",
     }),
   ],
+  callbacks:{
+    async redirect({ url, baseUrl }) {
+        return `${process.env.NEXT_PUBLIC_NEXT_AUTH_URL}/Pagess/sign/signIn/signIn`
+      },
+  },
   secret: process.env.JWT_SECRET,
 });
